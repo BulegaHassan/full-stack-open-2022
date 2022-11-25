464e9144aa57e81badc6f13a8c640c048b98619e
Hassan Bulega <hassanbulega@gmail.com>
2022-11-25 10:41:14 +0300

    add exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,21 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
   const [newName, setNewName] = useState("");
   const addName = (e) => {
     e.preventDefault();
     const newPerson = {
       name: newName,
     };
+
+    const index = persons.find((person) => person.name === newPerson.name);
+
+    // console.log("index", index);
+    if (index) {
+      alert(`${newPerson.name} is already added to phonebook`);
+      return;
+    }
     setPersons(persons.concat(newPerson));
     setNewName("");
   };
@@ -17,6 +25,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -25,12 +34,10 @@ const App = () => {
           <button type='submit'>add</button>
         </div>
       </form>
-        <h2>Numbers</h2>
-      
-        {persons.map((person) => (
-          <p key={person.name}>{person.name} </p>
-        ))}
-      
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name} </p>
+      ))}
     </div>
   );
 };
