bd86398a6dd50981b53d408a8f9a8fb737151e97
Hassan Bulega <hassanbulega@gmail.com>
2022-12-19 10:31:11 +0300

    add exercise 4.5

--- part4/blogList/tests/blog.test.js
+++ part4/blogList/tests/blog.test.js
@@ -55,3 +55,38 @@ describe("total likes", () => {
      expect(result).toBe(20);
    });
 });
+describe('the fav blog', ()=> {
+   const biggerList = [
+     {
+       _id: "5a422aa71b54a676234d17f8",
+       title: "Go To Statement Considered Harmful",
+       author: "Edsger W. Dijkstra",
+       url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+       likes: 5,
+       __v: 0,
+     },
+     {
+       title: "fsopen donee",
+       author: "matt finland",
+       url: "https://fullstackopen.com/en/part4/structure_of_backend_application_introduction_to_testing",
+       likes: 12,
+       id: "639d805feca7fbde1b2405af",
+     },
+     {
+       title: "fsopen again n' again",
+       author: "hassan bulega",
+       url: "https://fullstackopen.com/en/part4/structure_of_backend_application_introduction_to_testing",
+       likes: 3,
+       id: "639d7b9e69353841fc130a21",
+     },
+   ];
+   test('favorite blog object', ()=> {
+  
+    const result = listHelper.favoriteBlog(biggerList);
+    expect(result).toEqual({
+      title: "fsopen donee",
+      author: "matt finland",
+      likes: 12,
+    });
+   })
+})
