e29ae63983a5b30ea99cd412c7464458b281fc25
Hassan Bulega <hassanbulega@gmail.com>
2022-12-24 09:35:23 +0300

    add exercise 4.17

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,22 +1,34 @@
 const blogsRouter = require("express").Router();
 
 const Blog = require("../model/blog");
+const User = require("../model/user");
 // get all blogs
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 // get all posts
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
-  const { title, url } = body;
+  const { title, url, author, likes } = body;
   if (!title || !url) {
     return response.status(400).end();
   }
-  const blog = new Blog(body);
+  const users = await User.find({});
+  const user = users[0];
+  const blog = new Blog({
+    url,
+    title,
+    author,
+    likes,
+    user: user._id,
+  });
 
   const savedBlog = await blog.save();
 
+  user.blogs = user.blogs.concat(savedBlog._id);
+
+  await user.save();
   response.status(201).json(savedBlog);
 });
 //delete a post
--- part4/blogList/model/blog.js
+++ part4/blogList/model/blog.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
+  url: String,
   title: { type: String, required: true },
   author: { type: String, required: true },
-  url: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
   likes: { type: Number, default: 0 },
 });
 
