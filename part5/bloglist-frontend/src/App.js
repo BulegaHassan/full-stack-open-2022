c22d1119adef5eece7ed526ed017ac483f25e17d
Hassan Bulega <hassanbulega@gmail.com>
2023-01-17 08:52:33 +0300

    add exercise 5.4

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
-
+import Notification from "./components/Notification";
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
@@ -11,6 +11,7 @@ const App = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [url, setUrl] = useState("");
+  const [notification, setNotification] = useState(null);
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
@@ -24,6 +25,13 @@ const App = () => {
     }
   }, []);
 
+  const notify = (message, type = "info") => {
+    setNotification({ message, type });
+    setTimeout(() => {
+      setNotification(null);
+    }, 3000);
+  };
+
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
@@ -34,17 +42,18 @@ const App = () => {
       setUser(user);
       blogService.setToken(user.token);
       window.localStorage.setItem("loggedBlogAppUser", JSON.stringify(user));
+      notify(`Logged in successfully`);
 
       setUsername("");
       setPassword("");
     } catch (error) {
-      console.log(error);
+      notify(`wrong username or password`, "alert");
     }
   };
   const handleLogout = () => {
     window.localStorage.removeItem("loggedBlogAppUser");
     setUser(null);
-    console.log("logged out");
+    notify(`Logged out successfully`);
   };
   const addBlog = (e) => {
     e.preventDefault();
@@ -55,10 +64,11 @@ const App = () => {
     };
     blogService.create(blogObject).then((returnedBlog) => {
       setBlogs(blogs.concat(returnedBlog));
-      setTitle('')
-      setAuthor('')
-      setUrl('')
+      setTitle("");
+      setAuthor("");
+      setUrl("");
     });
+    notify(`a new blog ${title} by ${author} added`);
   };
   const loginForm = () => (
     <form onSubmit={handleLogin}>
@@ -90,15 +100,36 @@ const App = () => {
       <h1>create new</h1>
       <div>
         title:
-        <input type='text' id='title' name='title' value={title} onChange={({target})=> setTitle(target.value)}/>
+        <input
+          type='text'
+          id='title'
+          name='title'
+          value={title}
+          autoComplete='off'
+          onChange={({ target }) => setTitle(target.value)}
+        />
       </div>
       <div>
         author:
-        <input type='text' id='author' name='author' value={author} onChange={({target})=> setAuthor(target.value)}/>
+        <input
+          type='text'
+          id='author'
+          name='author'
+          value={author}
+          autoComplete='off'
+          onChange={({ target }) => setAuthor(target.value)}
+        />
       </div>
       <div>
         url:
-        <input type='text' id='url' name='url' value={url} onChange={({target})=> setUrl(target.value)}/>
+        <input
+          type='text'
+          id='url'
+          name='url'
+          value={url}
+          autoComplete='off'
+          onChange={({ target }) => setUrl(target.value)}
+        />
       </div>
       <button type='submit'>create</button>
     </form>
@@ -107,6 +138,7 @@ const App = () => {
     return (
       <div>
         <h2>Log in to application</h2>
+        <Notification notification={notification} />
         {loginForm()}
       </div>
     );
@@ -114,6 +146,7 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <Notification notification={notification} />
       <p>
         {user.name} logged in <button onClick={handleLogout}>logout</button>
       </p>
@@ -124,5 +157,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
