71b47f9681020fe86cba137570f3796bc4b8b636
Hassan Bulega <hassanbulega@gmail.com>
2023-02-20 14:42:59 +0300

    add exercise 6.12

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -5,11 +5,12 @@ import { Provider } from "react-redux";
 import App from "./App";
 import anecdoteReducer from "./reducers/anecdoteReducer";
 import filterReducer from "./reducers/filterReducer";
-
+import notificationReducer from "./reducers/notificationReducer";
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
     filter: filterReducer,
+    notification: notificationReducer,
   },
 });
 
---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = "";
+const notificationSlice = createSlice({
+  name: "notification",
+  initialState: "",
+  reducers: {
+    showNotification(state, action) {
+      const notification = action.payload;
+      return notification;
+    },
+  },
+});
+export const { showNotification } = notificationSlice.actions;
+export default notificationSlice.reducer;
